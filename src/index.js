368dd60fb82bce6ccdb1b5c62137d0878b73a232
Luis Guerra <luisad86@gmail.com>
2026-01-16 01:41:34 -0600

    refactor: remove Vercel serverless function entry point from index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Vercel serverless function entry point
-// This file exports the Express app for Vercel to handle requests
-
-// Import the built app from dist
-import app from '../dist/app.js';
-
-// Export the app as a serverless function handler
-export default app;
