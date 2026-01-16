017e6697718a83f5d58cc8dcc3f6fccfa0d826bb
Luis Guerra <luisad86@gmail.com>
2026-01-16 01:17:49 -0600

    refactor: update entry point in package.json and add main.ts for Express app initialization

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // This file exports the Express app for Vercel to handle requests
 
 // Import the built app from dist
-const app = require('../dist/app.js');
+import app from '../dist/app.js';
 
 // Export the app as a serverless function handler
-module.exports = app.default || app;
+export default app;
